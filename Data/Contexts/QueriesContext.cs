b3aba51fd8e776628d199b351e44f42d52d94307
mohamad.alturky <m799678@gmail.com>
2024-12-19 23:49:03 +0300

    adding execute procedure and function

--- Data/Contexts/QueriesContext.cs
+++ Data/Contexts/QueriesContext.cs
@@ -8,10 +8,13 @@ namespace Data.Contexts;
 
 public static class QueriesContext
 {
-    ////////////
-    ///      ///
-    ////////////
+    ///////////////
+    ///
+    ///////////////
 
+
+
+    #region from sql
     public static async Task<TTable?> Get<TTable>(
         string sql,
         object? parameters = null) where TTable : ITable
@@ -27,12 +30,14 @@ public static class QueriesContext
         using var connection = DbConnectionFactory.GetSqlServerConnection;
         return await connection.QueryAsync<TTable>(sql, parameters);
     }
+    #endregion
     ///
     ///
     ///
     ///
     ///
     ///
+    #region from sql with connection
     public static async Task<TTable?> Get<TTable>(
         IDbConnection connection,
         string sql,
@@ -48,13 +53,15 @@ public static class QueriesContext
     {
         return await connection.QueryAsync<TTable>(sql, parameters);
     }
+    #endregion
+    ///
+    ///
+    ///
+    ///
+    ///
+    ///
 
-    ///
-    ///
-    ///
-    ///
-    ///
-    ///
+    #region Directly
     public static async Task<TTable?> Get<TKey, TTable>(TKey id)
     where TTable : ITable<TKey>, new()
     {
@@ -63,19 +70,21 @@ public static class QueriesContext
         using var connection = DbConnectionFactory.GetSqlServerConnection;
         return await connection.QueryFirstOrDefaultAsync<TTable>(sql, new { Id = model.Id });
     }
-    public static async Task<IEnumerable<TTable>> List<TTable>() where TTable : ITable,new()
+    public static async Task<IEnumerable<TTable>> List<TTable>() where TTable : ITable, new()
     {
         TTable model = new();
         string sql = SqlQueryBuilder.GenerateSelectAllQuery(model.TableName, model.ColumnsWithOutId);
         using var connection = DbConnectionFactory.GetSqlServerConnection;
         return await connection.QueryAsync<TTable>(sql);
     }
+    #endregion
     ///
     ///
     ///
     ///
     ///
     ///
+    #region Directly With connection
     public static async Task<TTable?> Get<TKey, TTable>(
         IDbConnection connection,
         TKey id)
@@ -85,10 +94,88 @@ public static class QueriesContext
         string sql = SqlQueryBuilder.GenerateSelectByIdQuery(model.TableName, model.ColumnsWithOutId);
         return await connection.QueryFirstOrDefaultAsync<TTable>(sql, new { Id = model.Id });
     }
-    public static async Task<IEnumerable<TTable>> List<TTable>(IDbConnection connection) where TTable : ITable,new()
+    public static async Task<IEnumerable<TTable>> List<TTable>(IDbConnection connection) where TTable : ITable, new()
     {
         TTable model = new();
         string sql = SqlQueryBuilder.GenerateSelectAllQuery(model.TableName, model.ColumnsWithOutId);
         return await connection.QueryAsync<TTable>(sql);
     }
+    #endregion
+
+    #region Procedure
+    public static async Task<T> ExecuteProcedureSingleAsync<T>(
+        string procedureName,
+        object? parameters = null)
+    {
+        using var connection = DbConnectionFactory.GetSqlServerConnection;
+        return await connection.QuerySingleAsync<T>(procedureName, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public static async Task<T> ExecuteProcedureSingleAsync<T>(
+        IDbConnection connection,
+        string procedureName,
+        object? parameters = null,
+        IDbTransaction? transaction = null)
+    {
+        return await connection.QuerySingleAsync<T>(procedureName, parameters, commandType: CommandType.StoredProcedure, transaction: transaction);
+    }
+    public static async Task<IEnumerable<T>> ExecuteProcedureAsync<T>(
+        string procedureName,
+        object? parameters = null)
+    {
+        using var connection = DbConnectionFactory.GetSqlServerConnection;
+        return await connection.QueryAsync<T>(procedureName, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public static async Task<IEnumerable<T>> ExecuteProcedureAsync<T>(
+        IDbConnection connection,
+        string procedureName,
+        object? parameters = null,
+        IDbTransaction? transaction = null)
+    {
+        return await connection.QueryAsync<T>(procedureName, parameters, commandType: CommandType.StoredProcedure, transaction: transaction);
+    }
+    #endregion
+
+
+
+    #region Function
+    public static async Task<T> ExecuteFunctionSingleAsync<T>(
+        string functionName,
+        object? parameters = null)
+    {
+        using var connection = DbConnectionFactory.GetSqlServerConnection;
+        var result = await connection.QuerySingleAsync<T>(functionName, parameters, commandType: CommandType.Text);
+        return result;
+    }
+
+    public static async Task<T> ExecuteFunctionSingleAsync<T>(
+        IDbConnection connection,
+        string functionName,
+        object? parameters = null,
+        IDbTransaction? transaction = null)
+    {
+        var result = await connection.QuerySingleAsync<T>(functionName, parameters, commandType: CommandType.Text, transaction: transaction);
+        return result;
+    }
+    public static async Task<IEnumerable<T>> ExecuteFunctionAsync<T>(
+        string functionName,
+        object? parameters = null)
+    {
+        using var connection = DbConnectionFactory.GetSqlServerConnection;
+        var result = await connection.QueryAsync<T>(functionName, parameters, commandType: CommandType.Text);
+        return result;
+    }
+
+    public static async Task<IEnumerable<T>> ExecuteFunctionAsync<T>(
+        IDbConnection connection,
+        string functionName,
+        object? parameters = null,
+        IDbTransaction? transaction = null)
+    {
+        var result = await connection.QueryAsync<T>(functionName, parameters, commandType: CommandType.Text, transaction: transaction);
+        return result;
+    }
+
+    #endregion
 }
